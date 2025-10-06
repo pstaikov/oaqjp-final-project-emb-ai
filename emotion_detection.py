fa71e345efd0e12b8bf2abcba5ad616cb9aa481f
Pavlin Staikov <staikovp@yahoo.com>
2025-10-06 07:12:30 -0400

    Format the output of the app

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,9 +1,14 @@
-import requests
+import requests, json
 
 def emotion_detector(text_to_analyse):
+    
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_text = { "raw_document": { "text": text_to_analyse } }
-    response = requests.post(url, json = input_text, headers=headers)
-    return response.text
     
+    response = requests.post(url, json = input_text, headers=headers)
+    formatted_resp = json.loads(response.text)
+    em_scores = formatted_resp['emotionPredictions'][0]['emotion'] 
+    dominant_emotion = max(em_scores, key=em_scores.get)
+    em_scores['dominant_emotion'] = dominant_emotion
+    return em_scores
